31dbb335558a572ffe7a213d4101c1a716a324d1
gabriel caires <gabrielcairesmar@gmail.com>
2010-07-03 00:03:23 -0300

    fix

--- Scripts/I18n/I18nUtil.cs
+++ Scripts/I18n/I18nUtil.cs
@@ -21,10 +21,7 @@ public class I18nUtil:MonoBehaviour {
 	{
 		_instance = this;
 		GameObject.DontDestroyOnLoad (this);
-		
 	}
-	
-
 	
 	private void internal_Init (string language_p)
 	{
@@ -32,23 +29,20 @@ public class I18nUtil:MonoBehaviour {
 		language = language_p;
 		I18n.setLanguage(language);
 		_data=new I18nReader( Resources.Load(I18n.LANGUAGE_FILE).ToString());
-		
 	}
 	
-	
 	private string internal_GetValue (string id_p)
-	{
-	   
+	{  
 		if (_data.ContainsKey (id_p))
 			return _data.GetText(id_p);
 		    return "Text with id \"" + id_p + "\" not found on language " + language;
 	}
-//	
+	
 	private string internal_GetValue (string id_p, params object[] parameters_p){
 		return String.Format (internal_GetValue (id_p), parameters_p);
 	}
 
-		public static string GetValue (string id_p)	{
+	public static string GetValue (string id_p)	{
 		return _instance.internal_GetValue (id_p);
 	}
 
@@ -57,11 +51,10 @@ public class I18nUtil:MonoBehaviour {
 		
 		return _instance.internal_GetValue (id_p, args);
 	}
-//	
+
 	public static void Init (string language)
 	{
 		_instance.internal_Init (language);
-	
 	}
 
 }
